9118db29ced6519562814affee53a8199741205a
Semyon Laryukov <sene4ka.ok@yandex.ru>
2017-05-11 08:28:43 +0300

    change function

--- main.js
+++ main.js
@@ -1,107 +1,111 @@
-'use strict';
-var userFirstName = 'Semyon';
-var userLastName = 'Laryukov';
-console.log(userFirstName + ' ' + userLastName);
+(function () {
+	'use strict';
 
-function isNumber(num) {
-	return isFinite(num) && num === parseInt(num, 10);
-}
+	var userFirstName = 'Semyon';
+	var userLastName = 'Laryukov';
 
-var triangleArea = function (side1, side2, side3) {
-	if (isNumber(side1) && isNumber(side2) && isNumber(side3)) {
-		var p = (side1 + side2 + side3) / 2;
+	console.log(userFirstName + ' ' + userLastName);
 
-		return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
-	} else {
-		throw new Error('Неверный тип');
+	function isNumber(num) {
+		return isFinite(num) && num === parseInt(num, 10);
 	}
-}
 
-console.log('Площадь треугольника ' + triangleArea(3, 4, 5));
+	var triangleArea = function (side1, side2, side3) {
+		if (isNumber(side1) && isNumber(side2) && isNumber(side3)) {
+			var p = (side1 + side2 + side3) / 2;
 
-var cities = ['Ryazan', 'Moscow', 'Paris'];
-
-function reverseArrrayWhile(a) {
-	if (Array.isArray(a)) {
-		var start = 0;
-		var end = a.length - 1;
-
-		while (start <= end) {
-			var temp = a[start];
-			a[start] = a[end];
-			a[end] = temp;
-			start++;
-			end--;
-		}
-
-		return a;
-	}
-	throw new Error('Неверный тип');
-}
-
-console.log(reverseArrrayWhile(cities));
-
-function reverseArrrayFor(a) {
-	if (Array.isArray(a)) {
-		var counter = a.length - 1;
-
-		for (var i = 0; i < a.length; i++) {
-			var temp = a[i];
-
-			a[i] = temp;
-			counter--;
-		}
-
-		return a;
-	}
-	throw new Error('Неверный тип');
-}
-
-console.log(reverseArrrayFor(cities));
-
-var increment = 100;
-// 100
-console.log(increment++);
-// 101
-console.log(increment);
-increment = 100;
-// 101
-console.log(++increment);
-// 101
-console.log(increment);
-
-function checkNumber(num) {
-	if (isNumber(num)) {
-		if (num = 0) {
-			return 'Ноль';
+			return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
 		} else {
-			if (num < 0) {
-				return 'Отрицательное';
-			} else {
-				return 'Положительное';
+			throw new Error('Неверный тип');
+		}
+	}
+
+	console.log('Площадь треугольника ' + triangleArea(3, 4, 5));
+
+	var cities = ['Ryazan', 'Moscow', 'Paris'];
+
+	function reverseArrrayWhile(a) {
+		if (Array.isArray(a)) {
+			var start = 0;
+			var end = a.length - 1;
+
+			while (start <= end) {
+				var temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
+				start++;
+				end--;
 			}
+
+			return a;
 		}
-	} else {
 		throw new Error('Неверный тип');
 	}
-}
 
-console.log(checkNumber(0));
+	console.log(reverseArrrayWhile(cities));
 
-var userName = prompt("Ваше имя?");
+	function reverseArrrayFor(a) {
+		if (Array.isArray(a)) {
+			var counter = a.length - 1;
 
-console.log('Имя пользователя ' + userName);
+			for (var i = 0; i < a.length; i++) {
+				var temp = a[i];
 
-function calculateFactorial(num) {
-	if (isNumber(num)) {
-		if (num === 0) {
-			return 1;
-		} else {
-			return num * calculateFactorial(num - 1);
+				a[i] = temp;
+				counter--;
+			}
+
+			return a;
 		}
-	} else {
-		throw new Error('Неверный тип')
+		throw new Error('Неверный тип');
 	}
-}
 
-console.log('Факториал = ' + calculateFactorial(1));
+	console.log(reverseArrrayFor(cities));
+
+	var increment = 100;
+	// 100
+	console.log(increment++);
+	// 101
+	console.log(increment);
+	increment = 100;
+	// 101
+	console.log(++increment);
+	// 101
+	console.log(increment);
+
+	function checkNumber(num) {
+		if (isNumber(num)) {
+			if (num = 0) {
+				return 'Ноль';
+			} else {
+				if (num < 0) {
+					return 'Отрицательное';
+				} else {
+					return 'Положительное';
+				}
+			}
+		} else {
+			throw new Error('Неверный тип');
+		}
+	}
+
+	console.log(checkNumber(0));
+
+	var userName = prompt("Ваше имя?");
+
+	console.log('Имя пользователя ' + userName);
+
+	function calculateFactorial(num) {
+		if (isNumber(num)) {
+			if (num === 0) {
+				return 1;
+			} else {
+				return num * calculateFactorial(num - 1);
+			}
+		} else {
+			throw new Error('Неверный тип')
+		}
+	}
+
+	console.log('Факториал = ' + calculateFactorial(1));
+}());
